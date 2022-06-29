f6ad6869d78f060c38493cb86a3e50cc20c3d8aa
Denis Moiseev <isdenmois@gmail.com>
2022-06-29 22:49:18 +0300

    Initial commit

---
+++ apps/electron/.electron-builder.config.js
@@ -0,0 +1,32 @@
+if (process.env.VITE_APP_VERSION === undefined) {
+  const now = new Date();
+  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${
+    now.getUTCMonth() + 1
+  }.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+}
+
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  directories: {
+    output: "dist",
+    buildResources: "buildResources",
+  },
+  files: ["layers/**/dist/**"],
+  extraMetadata: {
+    version: process.env.VITE_APP_VERSION,
+  },
+  win: {
+    target: [
+      {
+        target: "7z",
+        arch: ["x64"],
+      },
+    ],
+    artifactName: "${productName}_${version}.${ext}",
+  },
+};
+
+module.exports = config;
